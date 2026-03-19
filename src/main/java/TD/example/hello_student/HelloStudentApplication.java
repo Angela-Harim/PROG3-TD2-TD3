1b5823e8b550d8e76dad5ddc86540feaae818baf
Angela-Haim <hei.angela.5@gmail.com>
2026-03-19 23:08:58 +0300

    Initial commit

---
+++ src/main/java/TD/example/hello_student/HelloStudentApplication.java
@@ -0,0 +1,13 @@
+package TD.example.hello_student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloStudentApplication.class, args);
+	}
+
+}
