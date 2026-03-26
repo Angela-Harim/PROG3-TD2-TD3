9b270074a11f4dc35a207f476b073fa47bac64d4
Angela-Haim <hei.angela.5@gmail.com>
2026-03-26 15:03:53 +0300

    feat: implement StudentService for in-memory data management

--- src/main/java/TD/example/hello_student/service/StudentService.java
+++ src/main/java/TD/example/hello_student/service/StudentService.java
@@ -1,4 +1,19 @@
 package TD.example.hello_student.service;
 
+import TD.example.hello_student.entity.Student;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class StudentService {
+    private List<Student> studentsInMemory = new ArrayList<>();
+
+    public void saveAll(List<Student> students) {
+        this.studentsInMemory.addAll(students);
+    }
+
+    public List<Student> getAll() {
+        return this.studentsInMemory;
+    }
 }
