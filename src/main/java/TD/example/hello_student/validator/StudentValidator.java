164e86a9d403a2c2801b75f1eb59c62a76b27503
Angela-Haim <hei.angela.5@gmail.com>
2026-03-26 15:01:07 +0300

    feat: implement StudentValidator to isolate validation logic

--- src/main/java/TD/example/hello_student/validator/StudentValidator.java
+++ src/main/java/TD/example/hello_student/validator/StudentValidator.java
@@ -1,4 +1,20 @@
 package TD.example.hello_student.validator;
 
+import TD.example.hello_student.entity.Student;
+import TD.example.hello_student.exception.BadRequestException;
+import org.springframework.stereotype.Component;
+
+@Component
 public class StudentValidator {
+    public void validate(Student student) {
+        if (student.getReference() == null || student.getReference().isBlank()) {
+            throw new BadRequestException("NewStudent.reference cannot be null");
+        }
+        if (student.getFirstName() == null || student.getFirstName().isBlank()) {
+            throw new BadRequestException("NewStudent.firstName cannot be null");
+        }
+        if (student.getLastName() == null || student.getLastName().isBlank()) {
+            throw new BadRequestException("NewStudent.lastName cannot be null");
+        }
+    }
 }
